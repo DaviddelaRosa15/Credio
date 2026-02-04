39616b128929d503506794ae167bcb2191e883f1
szxro <szxrocode@gmail.com>
2026-02-04 16:39:01 -0400

    chore(context): refactor the identity context and add new entities

--- Credio.Infrastructure.Identity/Contexts/IdentityContext.cs
+++ Credio.Infrastructure.Identity/Contexts/IdentityContext.cs
@@ -9,32 +9,57 @@ namespace Credio.Infrastructure.Identity.Contexts
 	{
 		public IdentityContext(DbContextOptions<IdentityContext> options) : base(options) { }
 
+		protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+		{
+			configurationBuilder.Properties<DateTime>().HaveColumnType("datetime2");
+			
+			base.ConfigureConventions(configurationBuilder);
+		}
+
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
-			//FLUENT API
+			modelBuilder.ApplyConfigurationsFromAssembly(typeof(IdentityContext).Assembly);
+			
 			base.OnModelCreating(modelBuilder);
-
-			modelBuilder.Entity<ApplicationUser>(entity =>
-			{
-				entity.ToTable(name: "Users");
-			});
-
-			modelBuilder.Entity<IdentityRole>(entity =>
-			{
-				entity.ToTable(name: "Roles");
-			});
-
-			modelBuilder.Entity<IdentityUserRole<string>>(entity =>
-			{
-				entity.ToTable(name: "UserRoles");
-			});
-
-			modelBuilder.Entity<IdentityUserLogin<string>>(entity =>
-			{
-				entity.ToTable(name: "UserLogins");
-			});
-
 		}
+		
+		public DbSet<Address> Address => Set<Address>();
+		
+		public DbSet<AmortizationMethod> AmortizationMethod => Set<AmortizationMethod>();
+		
+		public DbSet<AmortizationSchedule> AmortizationSchedule => Set<AmortizationSchedule>();
+		
+		public DbSet<AmortizationStatus> AmortizationStatus => Set<AmortizationStatus>();
+		
+		public DbSet<ApplicationStatus> ApplicationStatus => Set<ApplicationStatus>();
+		
+		public DbSet<Client> Client => Set<Client>();
+		
+		public DbSet<Employee> Employee => Set<Employee>();
+		
+		public DbSet<LateFee> LateFee => Set<LateFee>();
+		
+		public DbSet<LateFeeStatus> LateFeeStatus => Set<LateFeeStatus>();
+		
+		public DbSet<Loan> Loan => Set<Loan>();
+		
+		public DbSet<LoanApplication> LoanApplication => Set<LoanApplication>();
+		
+		public DbSet<LoanBalance> LoanBalance => Set<LoanBalance>();
+		
+		public DbSet<LoanStatus> LoanStatus => Set<LoanStatus>();
+		
+		public DbSet<Payment> Payment => Set<Payment>();
+		
+		public DbSet<PaymentFrequency> PaymentFrequency => Set<PaymentFrequency>();
+		
+		public DbSet<PaymentMethod> PaymentMethod => Set<PaymentMethod>();
+		
+		public DbSet<PaymentStatus> PaymentStatus => Set<PaymentStatus>();
+		
+		public DbSet<Route> Route => Set<Route>();
+		
+		public DbSet<SystemSettings> SystemSettings => Set<SystemSettings>();	
 
         public void TruncateTables()
         {
