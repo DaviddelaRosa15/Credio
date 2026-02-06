fbdc3e241a1b3727432b34defb8d5c70fb099654
szxro <szxrocode@gmail.com>
2026-02-06 17:05:19 -0400

    chore(extensions): add extension methods for middleware, Result and interfaces

---
+++ Credio.Authentication.Api/Extensions/InterfaceExtensions.cs
@@ -0,0 +1,71 @@
+using System.Text.Json.Serialization;
+using Credio.Authentication.Api.ExceptionHandler;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Credio.Interface.Authentication.Extensions;
+
+public static class InterfaceExtensions
+{
+    public static IServiceCollection AddInterfaceLayer(this IServiceCollection services)
+    {
+        // Core
+        services
+            .AddProblemDetails()
+            .AddLogging()
+            .AddMiddlewares()
+            .AddEndpointsApiExplorer()
+            .AddSwaggerGen();
+        
+        services.AddControllers(options =>
+            {
+                options.Filters.Add(new ProducesAttribute("application/json"));
+            })
+            .AddJsonOptions(x =>
+            {
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+            });
+        
+        /*
+         *.ConfigureApiBehaviorOptions(options =>
+           {
+               options.SuppressInferBindingSourcesForParameters = true;
+               options.SuppressMapClientErrors = false;
+               options.SuppressModelStateInvalidFilter = false;
+           })
+         * 
+         */
+
+        services.AddHealthChecks();
+        services.AddSwaggerExtension();
+        services.AddApiVersioningExtension();
+        services.AddDistributedMemoryCache();
+        services.AddSession(options =>
+        {
+            options.Cookie.Name = "MiSesion";
+            options.Cookie.HttpOnly = true;
+            options.Cookie.SameSite = SameSiteMode.None;
+            options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+        });
+
+        services.AddSignalR();
+        
+        // Exception Handlers
+        services.AddExceptionHandler<GlobalExceptionHandler>();
+        
+        // Cors
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowSpecificDomain",
+                builder =>
+                {
+                    builder.WithOrigins("http://localhost:5173");
+                    builder.AllowAnyMethod();
+                    builder.AllowAnyHeader();
+                    builder.AllowCredentials();
+                });
+        });
+        
+        return services;
+    }
+}
