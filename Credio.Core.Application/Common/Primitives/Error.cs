b076d7df9aa184472844f37efc514531525e5e22
szxro <szxrocode@gmail.com>
2026-02-06 16:26:16 -0400

    chore(primitives): add result, error and validation error primitives

---
+++ Credio.Core.Application/Common/Primitives/Error.cs
@@ -0,0 +1,30 @@
+using Credio.Core.Application.Enums;
+
+namespace Credio.Core.Application.Common.Primitives;
+
+public class Error
+{
+    public string ErrorCode { get; }
+
+    public string Description { get; }
+
+    public ErrorType Type { get; }
+
+    protected Error(
+        string errorCode,
+        string description,
+        ErrorType type)
+    {
+        ErrorCode = errorCode;
+        Description = description;
+        Type = type;
+    }
+
+    public static Error None = new Error(string.Empty, string.Empty, ErrorType.None);
+
+    public static Error Validation(string description) => new Error("Error.Validation", description, ErrorType.Validation);
+
+    public static Error Conflict(string description) => new Error("Error.Conflict", description, ErrorType.Conflict);
+
+    public static Error NotFound(string description) => new Error("Error.NotFound", description, ErrorType.NotFound);
+}
---
+++ Credio.Core.Application/Common/Primitives/ValidationError.cs
@@ -0,0 +1,16 @@
+using Credio.Core.Application.Enums;
+
+namespace Credio.Core.Application.Common.Primitives;
+
+public class ValidationError : Error
+{
+    public ValidationError(Error[] errors)
+        : base("Validation.Error",
+            "One or more validation errors occurred.",
+            ErrorType.Validation)
+    {
+        Errors = errors;
+    }
+
+    public Error[] Errors { get; set; }
+}
