2b8744cfef62d9877c2b281d70a7715c9d4d64c8
szxro <szxrocode@gmail.com>
2026-02-17 14:55:23 -0400

    refactor: now the properties of the address dto are going to be optional

--- Credio.Core.Application/Dtos/Common/AddressDTO.cs
+++ Credio.Core.Application/Dtos/Common/AddressDTO.cs
@@ -3,10 +3,10 @@
     public class AddressDTO
     {
         public string? StreetNumber { get; set; }
-        public string AddressLine1 { get; set; }
+        public string? AddressLine1 { get; set; }
         public string? AddressLine2 { get; set; }
-        public string City { get; set; }
-        public string Region { get; set; }
+        public string? City { get; set; }
+        public string? Region { get; set; }
         public string? PostalCode { get; set; }
     }
 }
