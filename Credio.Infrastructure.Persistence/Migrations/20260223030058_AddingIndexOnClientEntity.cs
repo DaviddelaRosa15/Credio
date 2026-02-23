eaa40c45d50096d1ad9e390ad3c4012ece6aec89
David N. de la Rosa L. <89267615+DaviddelaRosa15@users.noreply.github.com>
2026-02-22 23:01:52 -0400

    Add indexes on Client first/last name
    Add EF Core migration 'AddingIndexOnClientEntity' that creates indexes IX_Client_FirstName, IX_Client_LastName and composite IX_Client_FullName on the Client table to improve name-based query performance. Update ApplicationContextModelSnapshot to reflect the new indexes. Also add Microsoft.EntityFrameworkCore.Design package to the Credio.Lending.Api project to support EF tooling and migrations.

---
+++ Credio.Infrastructure.Persistence/Migrations/20260223030058_AddingIndexOnClientEntity.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Credio.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingIndexOnClientEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Client_FirstName",
+                table: "Client",
+                column: "FirstName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Client_FullName",
+                table: "Client",
+                columns: new[] { "FirstName", "LastName" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Client_LastName",
+                table: "Client",
+                column: "LastName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Client_FirstName",
+                table: "Client");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Client_FullName",
+                table: "Client");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Client_LastName",
+                table: "Client");
+        }
+    }
+}
