cacaea3520e7ac66b4cf45c9470c8b6f9851bd77
szxro <szxrocode@gmail.com>
2026-02-26 11:12:09 -0400

    feature: add loan application and application status interfaces and implementation

---
+++ Credio.Core.Application/Interfaces/Repositories/ILoanApplicationRepository.cs
@@ -0,0 +1,5 @@
+using Credio.Core.Domain.Entities;
+
+namespace Credio.Core.Application.Interfaces.Repositories;
+
+public interface ILoanApplicationRepository : IGenericRepository<LoanApplication> { }
