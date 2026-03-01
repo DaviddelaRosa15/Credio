6c97cadfbbe21ff5659038960ff4c223b09c55dd
Robt291610 <158675065+Robt291610@users.noreply.github.com>
2026-03-01 13:15:15 -0400

    Get employee by id endpoint added, now it is possible to filter clients by oficerId and also GetAll loansn added

---
+++ Credio.Lending.Api/Controllers/LoanController.cs
@@ -0,0 +1,44 @@
+﻿using Credio.Core.Application.Dtos.LoanApplication;
+using Credio.Core.Application.Features.LoanApplication;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using Credio.Interface.Lending.Extensions;
+using Credio.Lending.Api.Common;
+
+
+
+namespace Credio.Lending.Api.Controllers
+{
+    [Route("api/v1/loan-application")]
+    [ApiController]
+    public class LoanController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public LoanController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpGet("all")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LoanApplicationDto))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ProblemDetails))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ProblemDetails))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ProblemDetails))]
+        [SwaggerOperation(
+            Summary = "Obtiene todos los prestamos, o filtra por numero de empleado",
+            Description = "Obtiene los préstamos registrados, o filtra por id de empleado"
+        )]
+        public async Task<IResult> GetAllLoans([FromQuery] GetAllLoanApplicationsQuery query, CancellationToken cancellationToken)
+        {
+            var result = await _sender.Send(query, cancellationToken);
+
+            return result.Match(
+              onSuccess: () => CustomResult.Success(result),
+              onFailure: CustomResult.Problem);
+        }
+    }
+}
