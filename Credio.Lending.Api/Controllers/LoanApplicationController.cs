885c991c4a67f8b200e0581b66c7b76af4aea8c3
szxro <szxrocode@gmail.com>
2026-03-04 10:47:49 -0400

    refactor(controllers): change the roles in the authorize attribute and add type to the produces response type

--- Credio.Lending.Api/Controllers/LoanApplicationController.cs
+++ Credio.Lending.Api/Controllers/LoanApplicationController.cs
@@ -26,12 +26,12 @@ public class LoanApplicationController : ControllerBase
         Summary = "Creacion de solicitud de prestamos",
         Description = "Crear solicitud de prestamo"
     )]
-    [Authorize(Roles = "Administrator,Officer")]
+    [Authorize(Roles = "Officer")]
     [HttpPost("create")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LoanApplicationDto))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest,  Type = typeof(ProblemDetails))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ProblemDetails))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ProblemDetails))]
     public async Task<IResult> CreateLoanApplication([FromBody] CreateLoanApplicationCommand command, CancellationToken cancellationToken)
     {
         Result<LoanApplicationDto> result = await _sender.Send(command, cancellationToken);
@@ -45,12 +45,12 @@ public class LoanApplicationController : ControllerBase
         Summary = "Aprobacion de solicitud de prestamos",
         Description = "Aprobar solicitud de prestamos"
     )]
-    [Authorize(Roles = "Administrator,Officer")]
+    [Authorize(Roles = "Officer")]
     [HttpPut("approve/{id}")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest,  Type = typeof(ProblemDetails))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type =  typeof(ProblemDetails))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type =  typeof(ProblemDetails))]
     public async Task<IResult> ApproveLoanApplication(string id,[FromBody] ApproveLoanApplicationRequest request, CancellationToken cancellationToken)
     {
         Result result = await _sender.Send(request.ToCommand(id), cancellationToken);
@@ -64,12 +64,12 @@ public class LoanApplicationController : ControllerBase
         Summary = "Rechazo de solicitud de prestamos",
         Description = "Rechazar solicitud de prestamos"
     )]
-    [Authorize(Roles = "Administrator,Officer")]
+    [Authorize(Roles = "Officer")]
     [HttpPut("reject/{id}")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ProblemDetails))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type =  typeof(ProblemDetails))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type =  typeof(ProblemDetails))]
     public async Task<IResult> RejectLoanApplication(string id,[FromBody] RejectLoanApplicationRequest request, CancellationToken cancellationToken)
     {
         Result result = await _sender.Send(request.ToCommand(id), cancellationToken);
