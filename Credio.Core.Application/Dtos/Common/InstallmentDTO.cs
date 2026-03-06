b1c8564058ce93179bf1b35300e23e91d0a38dd1
David N. de la Rosa L. <89267615+DaviddelaRosa15@users.noreply.github.com>
2026-03-06 00:10:14 -0400

    Add amortization calculator service, interface and DTO
    Introduce amortization functionality: add InstallmentDTO, IAmortizationCalculatorService and AmortizationCalculatorService. The service implements a French-system amortization schedule (Calculate method) that computes period interest using a 360-day year assumption, calculates fixed installments, rounds amounts to 2 decimals, and adjusts the final installment to absorb cent rounding differences. Also remove the redundant Services folder entry from the project file. This adds a reusable API to generate installment schedules (approvedAmount, interestRate, termInInstallments, firstPaymentDate, daysInterval).

---
+++ Credio.Core.Application/Dtos/Common/InstallmentDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Credio.Core.Application.Dtos.Common
+{
+    public record InstallmentDTO(
+        int InstallmentNumber,
+        DateTime DueDate,
+        decimal DueAmount,
+        decimal PrincipalAmount,
+        decimal InterestAmount,
+        decimal RemainingBalance
+     );
+}
---
+++ Credio.Core.Application/Interfaces/Services/IAmortizationCalculatorService.cs
@@ -0,0 +1,10 @@
+﻿using Credio.Core.Application.Dtos.Common;
+
+namespace Credio.Core.Application.Interfaces.Services
+{
+    public interface IAmortizationCalculatorService
+    {
+        List<InstallmentDTO> Calculate(decimal approvedAmount, decimal interestRate, int termInInstallments,
+            DateTime firstPaymentDate, int daysInterval);
+    }
+}
