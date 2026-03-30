44648e7651a773f46397b3598d407ea3afa29da2
szxro <szxrocode@gmail.com>
2026-03-30 16:28:46 -0400

    feature: add new query to the loan repository

--- Credio.Core.Application/Interfaces/Repositories/ILoanRepository.cs
+++ Credio.Core.Application/Interfaces/Repositories/ILoanRepository.cs
@@ -8,8 +8,17 @@ public interface ILoanRepository : IGenericRepository<Loan>
     Task<int> GetLastLoanNumberAsync();
 
     Task<PortfolioSummaryDto?> GetPortfolioSummary(
-        string? statusId, string? searchTerm, DateOnly? startDate, DateOnly? endDate,
+        string? statusId,
+        string? searchTerm,
+        DateOnly? startDate,
+        DateOnly? endDate,
         CancellationToken cancellationToken = default);
 
     Task<List<Loan>> GetActiveLoansByDocumentNumberAsync(string documentNumber, CancellationToken cancellationToken = default);
+    
+    Task<CollectorPortfolioResponseDto>  GetCollectorPortfolio(
+        string employeeId,
+        string? searchTerm,
+        string? state,
+        CancellationToken cancellationToken = default);
 }
