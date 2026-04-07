5a0ab44965bf303dc2ec5ebbb55d7dbb94b07564
Robt291610 <158675065+Robt291610@users.noreply.github.com>
2026-04-06 20:39:27 -0400

    Seed payment status

---
+++ Credio.Core.Application/Constants/PaymentStatuses.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Credio.Core.Application.Constants
+{
+    public static class PaymentStatuses
+    {
+        public const string Completado = "Completado";
+        public const string Pendiente = "Pendiente";
+        public const string Anulado = "Anulado";
+    }
+}
---
+++ Credio.Core.Application/Seeds/DefaultPaymentStatus.cs
@@ -0,0 +1,45 @@
+﻿using Credio.Core.Application.Constants;
+using Credio.Core.Application.Interfaces.Repositories;
+using Credio.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Credio.Core.Application.Seeds
+{
+    public static class DefaultPaymentStatus
+    {
+        public static async Task SeedAsync(IPaymentStatusRepository paymentStatusRepository)
+        {
+            List<PaymentStatus> list = new();
+            try
+            {
+                var paymentStatus = new List<string>
+                    {
+                        PaymentStatuses.Completado,
+                        PaymentStatuses.Pendiente,
+                        PaymentStatuses.Anulado
+                    };
+
+                foreach (var item in paymentStatus)
+                {
+                    PaymentStatus status = new()
+                    {
+                        Name = item,
+                        Description = item
+                    };
+
+                    list.Add(status);
+                }
+
+                await paymentStatusRepository.AddManyAsync(list);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
