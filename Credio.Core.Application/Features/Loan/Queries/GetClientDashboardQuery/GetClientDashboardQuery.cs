425127a913beeda828a2a65905dbf6909544675c
szxro <szxrocode@gmail.com>
2026-04-10 19:03:53 -0400

    refactor: add new logic and change the way to retrieve some information to the database

--- Credio.Core.Application/Features/Loan/Queries/GetClientDashboardQuery/GetClientDashboardQuery.cs
+++ Credio.Core.Application/Features/Loan/Queries/GetClientDashboardQuery/GetClientDashboardQuery.cs
@@ -2,6 +2,7 @@ using Credio.Core.Application.Common.Primitives;
 using Credio.Core.Application.Dtos.Loan;
 using Credio.Core.Application.Interfaces.Abstractions;
 using Credio.Core.Application.Interfaces.Repositories;
+using Credio.Core.Application.Interfaces.Services;
 
 namespace Credio.Core.Application.Features.Loan.Queries.GetClientDashboardQuery;
 
@@ -14,25 +15,47 @@ public class GetClientDashboardQueryHandler : IQueryHandler<GetClientDashboardQu
 {
     private readonly IClientRepository _clientRepository;
     private readonly ILoanRepository _loanRepository;
+    private readonly ICurrentUserService _currentUserService;
 
     public GetClientDashboardQueryHandler(
         IClientRepository  clientRepository,
-        ILoanRepository  loanRepository)
+        ILoanRepository  loanRepository, 
+        ICurrentUserService currentUserService)
     {
         _clientRepository = clientRepository;
         _loanRepository = loanRepository;
+        _currentUserService = currentUserService;
     }
     public async Task<Result<ClientDashboardResponseDTO>> Handle(GetClientDashboardQuery request, CancellationToken cancellationToken)
     {
         try
         {
+            string? currentClientId = _currentUserService.GetCurrentUserId();
+
+            if (string.IsNullOrEmpty(currentClientId) || string.IsNullOrWhiteSpace(currentClientId))
+            {
+                return Result<ClientDashboardResponseDTO>.Failure(Error.BadRequest("Sesión no válida o expirada"));
+            }
+            
+            if (!_currentUserService.isInRole("Administrator") && currentClientId != request.ClientId)
+            {
+                return Result<ClientDashboardResponseDTO>.Failure(Error.BadRequest("No tienes permiso para ver el dashboard de otro cliente"));
+            }
+            
+            (int activeLoans, double totalBorrowed, double outstandingBalance) = await _loanRepository.GetClientLoanSummaryByClientIdAsync(request.ClientId, cancellationToken);
+
             OfficerInfoDTO? officeInfo = await _clientRepository.GetOfficerInfoByClientId(request.ClientId, cancellationToken);
             
-            ClientDashboardResponseDTO clientDashBoard = await _loanRepository.GetClientDashboard(request.ClientId, cancellationToken);
+            List<ClientDashboardLoanDTO> clientLoansOverview = await _loanRepository.GetClientLoansOverviewByClientId(request.ClientId, cancellationToken);
             
-            clientDashBoard.OfficerInfo = officeInfo;
-            
-            return Result<ClientDashboardResponseDTO>.Success(clientDashBoard);
+            return Result<ClientDashboardResponseDTO>.Success(new ClientDashboardResponseDTO
+            {
+                ActiveLoans = activeLoans,
+                TotalBorrowed = totalBorrowed,
+                OutstandingBalance = outstandingBalance,
+                OfficerInfo = officeInfo,
+                Loans =  clientLoansOverview
+            });
         }
         catch 
         {
