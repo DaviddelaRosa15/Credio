281a9f10a6faa1c6372a8dac8916989e512d0053
szxro <szxrocode@gmail.com>
2026-04-14 13:42:19 -0400

    feature(controller): add endpoint to get the application status by document number

--- Credio.Lending.Api/Controllers/BotController.cs
+++ Credio.Lending.Api/Controllers/BotController.cs
@@ -1,7 +1,9 @@
 using Credio.Core.Application.Common.Primitives;
 using Credio.Core.Application.Dtos.Loan;
+using Credio.Core.Application.Dtos.LoanApplication;
 using Credio.Core.Application.Dtos.LoanStatus;
 using Credio.Core.Application.Features.Loan.Queries.GetNextPaymentSummaryByDocumentQuery;
+using Credio.Core.Application.Features.LoanApplication.Queries.GetApplicationStatusForBotQuery;
 using Credio.Core.Application.Features.LoanStatus.Queries.GetLoanStatuses;
 using Credio.Interface.Lending.Extensions;
 using Credio.Lending.Api.Common;
@@ -40,5 +42,22 @@ public class BotController : ControllerBase
         return result.Match(
             onSuccess: () => CustomResult.Success(result),
             onFailure: CustomResult.Problem);
+    }
+    
+    [Authorize(Roles = "Administrator, Officer")]
+    [HttpGet("applications/status/{documentNumber}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<BotApplicationStatusDTO>))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ProblemDetails))]
+    [SwaggerOperation(
+        Summary = "Consulta que devuelva el estatus actual de las solicitudes recientes de un cliente",
+        Description = "Consulta que devuelva el estatus actual de las solicitudes recientes de un cliente"
+    )]
+    public async Task<IResult> GetApplicationStatusByDocumentNumber(string documentNumber, CancellationToken cancellationToken = default)
+    {
+        Result<List<BotApplicationStatusDTO>> result = await _sender.Send(new GetApplicationStatusForBotQuery{DocumentNumber = documentNumber}, cancellationToken);
+        
+        return result.Match(
+            onSuccess: () => CustomResult.Success(result),
+            onFailure:CustomResult.Problem);
     }
 }
