152939033a48bf41cf1ec0a62066608d5ec20c93
David N. de la Rosa L. <89267615+DaviddelaRosa15@users.noreply.github.com>
2026-04-21 00:04:40 -0400

    Add loan disbursement email helper and template
    Introduce MakeEmailForLoanDisbursement to IEmailHelper and its implementation, which renders a LoanDisbursement template using DisburseLoanNotificationDTO (ClientName, LoanNumber, DisbursementDate, DisbursedAmount). Add LoanDisbursement.html email template (Spanish) with placeholders for those values and layout/assets (cid:logoCid). Also add required DTO using directive in the helper.

--- Credio.Core.Application/Interfaces/Helpers/IEmailHelper.cs
+++ Credio.Core.Application/Interfaces/Helpers/IEmailHelper.cs
@@ -1,5 +1,6 @@
 using Credio.Core.Application.Dtos.CoreConfiguration;
 using Credio.Core.Application.Dtos.Email;
+using Credio.Core.Application.Dtos.Loan;
 using Credio.Core.Application.Dtos.Payment;
 
 namespace Credio.Core.Application.Interfaces.Helpers
@@ -9,6 +10,7 @@ namespace Credio.Core.Application.Interfaces.Helpers
         string MakeEmailForChange(string fullName);
         string MakeEmailForEmployee(EmployeeWelcomeEmail employee);
         string MakeEmailForLoanArrearsNotice(LoanInArrearsNotificationDTO notification);
+        string MakeEmailForLoanDisbursement(DisburseLoanNotificationDTO notification);
         string MakeEmailForReset(string fullName, string code);
         string MakeEmailForEodAlert(EodAlertNotificationDTO notification);
         string MakeEmailForPaymentNotifications(PaymentNotificationDTO notification);
